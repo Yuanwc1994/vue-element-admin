f81ccefedc338f4393b05420145b884cb2d765cd
yuanwenchao <867420230@qq.com>
2019-10-10 10:11:20 +0800

    抽离前置路由守卫

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import './styles/index.scss'
 import axios from './config/httpConfig'
 import * as globalFilter from './filters/filters'
 import '@/icons'
+import '@/router/permission'
 
 Vue.prototype.$http = axios
 // 创建事件总线
@@ -21,35 +22,8 @@ for (var key in globalFilter) {
 
 Vue.use(ElementUI)
 
+// 阻止启动生产消息
 Vue.config.productionTip = false
-
-router.beforeEach((to, from, next) => {
-    if (!store.state.UserInfo) {
-        if (to.matched.length > 0 && !to.matched.some(record => record.meta.requiresAuth)) {
-            next()
-        } else {
-            next({ path: '/login' })
-        }
-    } else {
-        if (!store.state.permission.permissionList) {
-            store.dispatch('permission/FETCH_PERMISSION').then(() => {
-                next({ path: to.path })
-            })
-        } else {
-            if (to.path !== '/login') {
-                next()
-            } else {
-                next(from.fullPath)
-            }
-        }
-    }
-})
-
-router.afterEach((to, from, next) => {
-    var routerList = to.matched
-    store.commit('setCrumbList', routerList)
-    store.commit('permission/SET_CURRENT_MENU', to.name)
-})
 
 /* eslint-disable no-new */
 new Vue({
